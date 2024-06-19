145b0e50bd93b5c25026ef1b8097b109ba61e126
Trung Hoang <hdh.trung96@gmail.com>
2024-06-19 16:30:41 +0700

    Update size default for 'EPrimaryHeaderContainer' widget

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,5 +1,4 @@
 import 'package:e_commerce_app/common/widgets/custom_shapes/containers/primary_header_container.dart';
-import 'package:e_commerce_app/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -7,17 +6,13 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
             //! Test phần layout hộp chứa tiêu đề
             EPrimaryHeaderContainer(
-              child: SizedBox(
-                height: EHelperFunctions.screenHeight() * 2 / 3,
-                width: double.infinity,
-                child: const Center(child: Text('H.O.M.E')),
-              ),
+              child: Center(child: Text('H.O.M.E')),
             ), //!
           ],
         ),
